ce395953b6eccd2852c5ebedecc10fc7a35712ac
mechztcc <dojathan@gmail.com>
2021-12-28 13:29:02 -0300

    config errors

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,15 +1,36 @@
 import "reflect-metadata";
-// import  '@shared/typeorm'
+import  '@shared/typeorm';
 
 import { Request, Response } from 'express';
 import express = require('express');
 import cors = require('cors');
+import AppError from "@shared/errors/AppError";
 
 const PORT = 8000;
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+
+
+app.use(
+    (error: Error, request: Request, response: Response, next: express.NextFunction) => {
+      if (error instanceof AppError) {
+        console.log(error);
+        return response
+          .status(error.statusCode)
+          .json({ status: 'error', message: error.message });
+          
+      }
+      console.log(error);
+      return response
+        .status(500)
+        .json({ status: 'error', message: 'Internal Server Error' });
+        
+    },
+  );
+
 
 app.get('/', (req: Request, res: Response) => {
     res.json(PORT)
